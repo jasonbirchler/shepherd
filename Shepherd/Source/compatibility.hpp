e30f4ef215e140ff2f7c8c64d65e65a7c865a4a3
Jason Birchler <hugenerd@mac.com>
2025-11-18 15:49:04 -0800

    another attempt at fixing std::exchange build failures

--- Shepherd/Source/compatibility.hpp
+++ Shepherd/Source/compatibility.hpp
@@ -1,21 +1,11 @@
-/*
-  ==============================================================================
-  
-    compatibility.hpp
-    Created: Compatibility fixes for third-party library issues
-  
-  ==============================================================================
-*/
-
 #pragma once
 
-// Include missing C++ standard library headers that some JUCE components need
-#include <utility>  // For std::exchange
+#include <utility>
 
-// If needed, provide a fallback std::exchange implementation for older compilers
-#if __cplusplus < 201402L && !defined(__cpp_lib_exchange)
+// Compatibility shim for older compilers/standard libraries
+#if !defined(__cpp_lib_exchange_function) || __cpp_lib_exchange_function < 201304L
 namespace std {
-    template<typename T, typename U>
+    template<class T, class U = T>
     constexpr T exchange(T& obj, U&& new_value) {
         T old_value = std::move(obj);
         obj = std::forward<U>(new_value);
